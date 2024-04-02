9d1ca8d0e91f068cfeae4d7b4e462b884bf31d86
sunsonni <sonniemnguyen@gmail.com>
2024-04-02 16:32:41 -0500

    Practice skills exercises done

--- more-on-functions/exercises/practice-your-skills.js
+++ more-on-functions/exercises/practice-your-skills.js
@@ -1,15 +1,13 @@
 //Create an anonymous function and set it equal to a variable.
-
-/* Your function should:
-a) If passed a number, return the tripled value.
-b) If passed a string, return the string “ARRR!”
-c) Be sure to test your function before moving on the next part. 
-*/
-
-/* Add to your code! Use your fuction and the map method to change an array  as follows:
-a) Triple any the numbers.
-b) Replace any strings with “ARRR!”
-c) Print the new array to confirm your work.
-*/
-
+let output = function(input) {
+    let finalOutput;
+    if (!isNaN(input)) {
+        finalOutput = input * 3;
+    } else {
+        finalOutput = "ARRR!";
+    }
+    return finalOutput;
+};
 let arr = ['Elocution', 21, 'Clean teeth', 100];
+let mappedArray = arr.map(output);
+console.log(mappedArray);
