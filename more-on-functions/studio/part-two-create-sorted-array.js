75884429795151c4fa4d976df15f3f71cb99daa5
sunsonni <sonniemnguyen@gmail.com>
2024-04-04 19:12:16 -0500

    part two more functions done

--- more-on-functions/studio/part-two-create-sorted-array.js
+++ more-on-functions/studio/part-two-create-sorted-array.js
@@ -1,3 +1,10 @@
+let nums1 = [5, 10, 2, 42];
+let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
+let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
+
+
+
+
 function findMinValue(arr){
   let min = arr[0];
   for (i = 0; i < arr.length; i++){
@@ -9,21 +16,31 @@ function findMinValue(arr){
 }
 
 //Create a function with an array of numbers as its parameter. This function will return a new array with the numbers sorted from least to greatest value.
+function newSortedArray(array) {
+  let sortedArray = [];
+  let minValIndex = 0;
 
-/*Within the function:
-1) Define a new, empty array to hold the final sorted numbers.
-2) Use the findMinValue function to find the minimum value in the old array.
-3) Add the minimum value to the new array, and remove the minimum value from the old array.
-4) Repeat parts b & c until the old array is empty.
-5) Return the new sorted array.
-6) Be sure to print the results in order to verify your code.*/
+  while(array.length > 0){
+    sortedArray.push(findMinValue(array));
+    minValIndex = array.indexOf(findMinValue(array));
+    array.splice(minValIndex, 1);
+  }
+  return sortedArray;
+} 
+// console.log(newSortedArray(nums3));
+// console.log(nums3);
 
 //Your function here...
+function recursiveFunction(array, newArray = [] ) {
+  if(array.length === 1){
+    newArray.push(array[0]);
+    return newArray;
+  } else {
+  newArray.push(findMinValue(array));
+  array.splice(array.indexOf(findMinValue(array)), 1);
+  recursiveFunction(array, newArray);
+  }
+  return newArray;
+}
 
-/* BONUS MISSION: Refactor your sorting function to use recursion below:
- */
-
-//Sample arrays for testing:
-let nums1 = [5, 10, 2, 42];
-let nums2 = [-2, 0, -10, -44, 5, 3, 0, 3];
-let nums3 = [200, 5, 4, 10, 8, 5, -3.3, 4.4, 0];
+console.log(recursiveFunction(nums2));
