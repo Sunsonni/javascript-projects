0d395d892adb233691a3fb8ee5d1b1227b8e0aa2
sunsonni <sonniemnguyen@gmail.com>
2024-04-08 19:06:58 -0500

    Objects Studio Part One done

--- objects-and-math/studio/ObjectsStudio01.js
+++ objects-and-math/studio/ObjectsStudio01.js
@@ -1,10 +1,34 @@
-// Code your selectRandomEntry function here:
-
-
-// Code your buildCrewArray function here:
-
+function selectRandomEntry(array){
+  let randSelect= Math.floor(Math.random()*array.length);
+  randSelect=array[randSelect];
+  return randSelect;
+}
 
 let idNumbers = [291, 414, 503, 599, 796, 890];
+
+let randArr =[];
+while(randArr.length < 3){
+  //call randSelect? 
+  let randCrewMember = selectRandomEntry(idNumbers);
+  if(!randArr.includes(randCrewMember)){
+    randArr.push(randCrewMember);
+} else {
+  console.log("crew member already in mission.")
+}
+}
+console.log(randArr);
+// console.log(randArr);
+// Code your buildCrewArray function here:
+function buildCrewArray(idArray,candArr){
+  let crewArr=[];
+  for(let i=0;i<candArr.length;i++){
+    //loops over candidate array
+    if(idArray.includes(candArr[i].astronautID)){
+      crewArr.push(candArr[i]);
+    }
+  }
+  return crewArr;
+}
 
 // Here are the candidates and the 'animals' array:
 let candidateA = {
@@ -49,7 +73,9 @@ let candidateF = {
   'o2Used':function(hrs){return 0.018*hrs},
   'astronautID':890
 };
-
 let animals = [candidateA,candidateB,candidateC,candidateD,candidateE,candidateF];
+let crew = buildCrewArray(randArr, animals);
 
 // Code your template literal and console.log statements:
+console.log(crew[0].astronautID, crew[1].astronautID, crew[2].astronautID);
+console.log(`${crew[0].name}, ${crew[1].name}, and ${crew[2].name} are going to space!`)
