05ea18ed1dc53c563368661b01963dfa6c6be9ad
sunsonni <sonniemnguyen@gmail.com>
2024-04-23 09:49:00 -0500

    DOM exercises complete

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -2,8 +2,26 @@ function init () {
     const missionAbort = document.getElementById("abortMission");
     const button = document.getElementById("liftoffButton");
     const paragraph = document.getElementById("statusReport");
+//Lift off button press event
+    button.addEventListener('click', event => {
+        paragraph.innerHTML =  'Houston! We have liftoff!'
+    });
+//Abort mission button mouse over event
+   missionAbort.addEventListener("mouseover", function (){
+        document.getElementById("abortMission").style.background='#FF0000';
+    });
+//Abort mission button mouse out event
+   missionAbort.addEventListener("mouseout", function (){
+        document.getElementById("abortMission").style.background='';
+    });
+//Abort mission buttton click event
+    missionAbort.addEventListener("click", function(){
+        let missionAborted = window.confirm("Are you sure you want to abort the mission?");
+        if(missionAborted){
+            paragraph.innerHTML =  'Mission Aborted! Space shuttle returning home.' 
+        }
+    });
 
-    // Put your code for the exercises here.
     
 }
 
